7363b73490c652b23e2720d89bcdd95a0f33eff0
Sayed Hamdi <32652375+sayedhamdi@users.noreply.github.com>
2023-09-27 07:37:52 +0100

    Add files via upload

---
+++ seance-8/e.js
@@ -0,0 +1,12 @@
+
+//input 
+let c = "cab"
+
+
+
+
+if(c[1] + c[0] + c[2] == "abc" || c[0] + c[2] + c[1] =="abc" || c[0] + c[2] + c[1]=="abc"){
+    console.log("YES")
+}else {
+    console.log("NO")
+}
---
+++ seance-8/main.js
@@ -0,0 +1,56 @@
+/*let x = 4
+let y = 2
+
+
+let result = x * y
+
+console.log("le resultat de "+ x +" * "+ y +" = " +result)
+
+let inputX = document.querySelector("#x")
+let inputY = document.querySelector("#y")
+let buttonMult = document.querySelector("#mult")
+let buttonAddition = document.querySelector("#add")
+
+
+let res = document.querySelector("#resultat")
+
+
+
+
+buttonMult.onclick = function(){
+    res.innerHTML = inputX.value * inputY.value
+} 
+buttonAddition.onclick = function(){
+    res.innerHTML = Number(inputX.value) + Number(inputY.value)
+} 
+
+*/
+
+let etudiant = ["Ahmed","Kenza","Yasmine","Nour","Jawher","Wael"]
+let presence = [true,true,true,true,false,false]
+
+
+for (let i = 0;i<etudiant.length;i++){
+    console.log(etudiant[i])
+}
+
+
+
+ 
+// taht el 0 bered barcha 
+// mel 0 el 10 bered
+
+// mel 10 - 30 ça va 
+
+// fou9 el 30 skhouna
+
+// fou9 el 45 bech nmoutou
+let temperature = 0
+
+
+if (temperature > 0){
+    console.log("taks ça va mriguel")
+}else {
+    console.log("taks bered ")
+}
+
